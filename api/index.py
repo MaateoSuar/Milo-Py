b2859648162a724d506be3d974768fdbb9e1a21b
Maateosuar <maateosuar@gmail.com>
2025-08-25 15:51:35 -0300

    Commit para Deploy

--- api/index.py
+++
@@ -1,4 +0,0 @@
-from app import app as application
-
-# Este archivo sirve como punto de entrada para Vercel
-# La variable 'application' debe estar definida para que Vercel pueda ejecutar la aplicación
--- wsgi.py
+++
@@ -1,6 +0,0 @@
-"""
-Archivo WSGI para el despliegue en Vercel
-"""
-from app import app as application
-
-# Este archivo es necesario para que Vercel pueda ejecutar la aplicación Python
